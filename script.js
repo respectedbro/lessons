73e0e2b3cdc99124192ad351531534a788258dc8
resAsus <respectedbro@yandex.ru>
2024-08-08 18:25:52 +0300

    hard

--- script.js
+++ script.js
@@ -6,24 +6,51 @@ const DomElement = function (selector, height, width, bg, fontSize) {
 	this.width = width
 	this.bg = bg
 	this.fontSize = fontSize
-	this.createElement = function() {
-		if(selector[0] === '.') {
-			let div = document.createElement('div')
-			div.classList.add(this.selector.slice(1))
-			div.style.cssText = `height: ${height}px; width: ${width}px; background-color: ${bg}; font-size: ${fontSize}px;`
-			div.textContent = 'Block'
-			document.body.append(div)
+	this.createElement = function () {
+		let elem
+		if (selector[0] === '.') {
+			elem = document.createElement('div')
+			elem.classList.add(this.selector.slice(1))
+			elem.style.cssText = `height: ${height}px; width: ${width}px; background-color: ${bg}; font-size: ${fontSize}px; position: absolute`
+			document.body.append(elem)
 		} else if (selector[0] === '#') {
-			let p = document.createElement('p')
-			p.id = this.selector.slice(1)
-			p.style.cssText = `height: ${height}px; width: ${width}px; background-color: ${bg}; font-size: ${fontSize}px;`
-			p.textContent = 'Best'
-			document.body.append(p)
+			elem = document.createElement('p')
+			elem.id = this.selector.slice(1)
+			elem.style.cssText = `height: ${height}px; width: ${width}px; background-color: ${bg}; font-size: ${fontSize}px;`
+			elem.textContent = 'Best'
+			document.body.append(elem)
 		}
+
+		return elem
 	}
 }
 
-let newObj1 = new DomElement('.block', 70, 80, 'yellow', 30)
-newObj1.createElement()
-let newObj2 = new DomElement('#best', 40, 50, 'green', 20)
-newObj2.createElement()
+// let newObj1 = new DomElement('.block', 100, 100, 'black')
+// newObj1.createElement()
+
+let newObj1 = new DomElement('.block', 100, 100, 'black')
+let elem = newObj1.createElement()
+
+let x = 0
+let y = 0
+
+document.addEventListener('keydown', function (event) {
+	let px = 10
+
+	switch (event.key) {
+		case 'ArrowUp':
+			y -= px
+			break
+		case 'ArrowDown':
+			y += px
+			break
+		case 'ArrowLeft':
+			x -= px
+			break
+		case 'ArrowRight':
+			x += px
+			break
+	}
+
+	elem.style.transform = `translate(${x}px, ${y}px)`
+})
