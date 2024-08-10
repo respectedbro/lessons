1ff4a8cc7ef711687bd61264b8180ebd7ff41fb2
resAsus <respectedbro@yandex.ru>
2024-08-10 12:44:54 +0300

    lesson16

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict'
+
+class First {
+	constructor() {}
+	hello() {
+		console.log('Привет я метод родителя')
+	}
+}
+
+class Second extends First {
+	constructor() {
+		super()
+	}
+	hello() {
+		super.hello()
+		console.log('А я наследуемый метод!')
+	}
+}
+const first = new First()
+const second = new Second()
+
+second.hello()
