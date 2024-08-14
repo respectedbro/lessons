7a563e9e95acf1f9fb55dded509d1a16c4264c15
resAsus <respectedbro@yandex.ru>
2024-08-14 23:56:46 +0300

    lesson19

--- script.js
+++ script.js
@@ -1,22 +1,82 @@
 'use strict'
 
-class First {
-	constructor() {}
-	hello() {
-		console.log('Привет я метод родителя')
+const days = [
+	'Понедельник',
+	'Вторник',
+	'Среда',
+	'Четверг',
+	'Пятница',
+	'Суббота',
+	'Воскресенье',
+]
+
+const addTimeZero = time => {
+	if (time < 10) {
+		return '0' + time
+	} else {
+		return time
 	}
 }
 
-class Second extends First {
-	constructor() {
-		super()
-	}
-	hello() {
-		super.hello()
-		console.log('А я наследуемый метод!')
+const formatTimeOfDay = hours => {
+	if (hours > 0 && hours < 6) {
+		return 'Доброй ночи'
+	} else if (hours > 6 && hours < 10) {
+		return 'Доброе утро'
+	} else if (hours > 10 && hours < 18) {
+		return 'Добрый день'
+	} else {
+		return 'Добрый вечер'
 	}
 }
-const first = new First()
-const second = new Second()
+const formatLocaleTime = hours => {
+	if (hours > 0 && hours < 12) {
+		return 'AM'
+	} else {
+		return 'PM'
+	}
+}
 
-second.hello()
+const getDaysUnNewYear = () => {
+	const now = new Date()
+	const nextYear = now.getFullYear() + 1
+	const newYear = new Date(nextYear, 0, 1)
+
+	const diffInTime = newYear - now
+	const diffInDays = Math.floor(diffInTime / (1000 * 60 * 60 * 24))
+
+	return diffInDays
+}
+
+let dateNow = document.createElement('span')
+document.body.append(dateNow)
+
+const updateTime = () => {
+	const date = new Date()
+
+	let hours = date.getHours()
+	let minutes = date.getMinutes()
+	let seconds = date.getSeconds()
+	let day = date.getDay()
+
+	let timeOfDay = formatTimeOfDay(hours)
+	let localeTime = formatLocaleTime(hours)
+	let daysUnNewYear = getDaysUnNewYear()
+
+	let formatHours = addTimeZero(hours)
+	let formatMinutes = addTimeZero(minutes)
+	let formatSeconds = addTimeZero(seconds)
+
+	if (day === 0) {
+		day = 6
+	} else {
+		day -= 1
+	}
+
+	dateNow.textContent = `${timeOfDay} Сегодня ${days[day]} Текущее время: ${formatHours}:${formatMinutes}:${formatSeconds}${localeTime}
+	до нового года осталось ${daysUnNewYear} дней`
+
+	setTimeout(updateTime, 1000)
+}
+
+updateTime()
